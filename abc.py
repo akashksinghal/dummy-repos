e91c4261a1579525d94621a5010acdddfbc65e69
Akash Kumar Singhal <akashksinghalnewdelhi@gmail.com>
2023-10-06 05:58:58 +0530

    fafa

--- abc.py
+++ abc.py
@@ -1,20 +1,17 @@
-from flask import Flask, request, jsonify
 import socket
 
-app = Flask(__name__)
+def get_ip_address(domain_name):
+    try:
+        ip_address = socket.gethostbyname(domain_name)
+        return ip_address
+    except socket.gaierror as e:
+        return f"Failed to resolve the IP address for '{domain_name}': {str(e)}"
 
-@app.route('/get_ip', methods=['GET'])
-def get_ip():
-    website_url = request.args.get('url')
+# Replace 'example.com' with the website's domain name you want to look up
+website_domain = 'example.com'
+ip_address = get_ip_address(website_domain)
 
-    if website_url:
-        try:
-            ip_address = socket.gethostbyname(website_url)
-            return jsonify({'ip_address': ip_address})
-        except socket.gaierror as e:
-            return jsonify({'error': f"Failed to resolve the IP address for '{website_url}': {str(e)}"}), 400
-    else:
-        return jsonify({'error': 'URL parameter is missing'}), 400
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if not ip_address.startswith("Failed"):
+    print(f"The IP address of {website_domain} is: {ip_address}")
+else:
+    print(ip_address)
